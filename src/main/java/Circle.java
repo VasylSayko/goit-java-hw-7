ade7d5840776e5c0eecb3d375b0d9a18eb57ad29
VasylSayko <v.sajko@outlook.com>
2021-02-05 15:00:13 +0200

    Create Shapes

---
+++ src/main/java/Circle.java
@@ -0,0 +1,8 @@
+public class Circle extends Shape{
+
+    @Override
+    public void printShapeName ()
+    {
+        System.out.println("This is Circle");
+    }
+}
---
+++ src/main/java/Shape.java
@@ -0,0 +1,5 @@
+public abstract class Shape implements ShapePrinter{
+
+    @Override
+    public abstract void printShapeName();
+}
